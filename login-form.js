f70f7ac5f22239f8a1d85cd284a3272aa29900e3
Xavier Agostini <xavier.agostini@mail.mcgill.ca>
2016-11-09 21:50:50 -0500

    add in popup styling

--- login-form.js
+++ login-form.js
@@ -1,13 +1,27 @@
+// Load in stored login details
 updateInfo();
 
+// Open up paycom login page
 $("#loginBtn").click(function() {
-	console.log("login");
-	// window.location.href = "https://www.paycomonline.net/v4/ee/ee-login.php";
 	chrome.tabs.update({url: "https://www.paycomonline.net/v4/ee/ee-login.php"});
-})
-$("#updateInfo").click(function() {
-	$("#infoPage").show();
 });
+
+// Open login details form
+$("#updateInfo").click(function() {
+	$(this).toggleClass("active");
+	if($(this).hasClass("active")) {
+		$("#infoPage").show();
+	} else {
+		$("#infoPage").hide();
+	}
+});
+
+// Redirect to github repository
+$("#repoBtn").click(function() {
+	chrome.tabs.update({url: "https://github.com/XavierAgostini/Paycom-Extension"});
+});
+
+// Store login info on login details form submission
 $("#loginInfo").submit(function(e) {
 	e.preventDefault();
 	var loginDetails = {
@@ -19,33 +33,25 @@ $("#loginInfo").submit(function(e) {
 	var loginInfo = {} ;
 	chrome.storage.sync.get(["loginInfo"], function(result) {
 		loginInfo = result.loginInfo;
-		console.log(result.loginInfo);
 	});
-	// window.location.href = "https://www.paycomonline.net/v4/ee/ee-login.php";
 	$("#infoPage").hide();
 });
 
-
-
+// Get stored login info and write it into login details form
 function updateInfo() {
 	// User Information
 	var loginInfo = {} ;
 	chrome.storage.sync.get(["loginInfo"], function(result) {
-		loginInfo = JSON.parse(result.loginInfo);
-		console.log(result.loginInfo);
-		if(loginInfo) {
-			console.log("inside");
+		if(result.loginInfo) {
+			loginInfo = JSON.parse(result.loginInfo);
 			var my_username = loginInfo.userID;
 			var my_password = loginInfo.userPass;
 			var my_pin = loginInfo.userPin;
 
-			$('input[name="userId"]').val(my_username),
-			$('input[name="userPass"]').val(my_password),
-			$('input[name="userPin"]').val(my_pin) 
-		}
-	});
-	
+			$('input[name="userId"]').val(my_username);
+			$('input[name="userPass"]').val(my_password);
+			$('input[name="userPin"]').val(my_pin);
+		} 
 
-	
-	
+	});	
 }
